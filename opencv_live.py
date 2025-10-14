0064d00d005f207aea822d972ba71f8acefbacd2
Tim Wijma <tim.wijma@gmail.com>
2025-10-14 11:31:13 +0200

    simple face detection

--- opencv_live.py
+++ opencv_live.py
@@ -4,6 +4,10 @@ import sys
 def main() -> None:
     cap = cv2.VideoCapture(0)  # 0 = default camera
 
+    face_classifier = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
     if not cap.isOpened():
         sys.exit("Could not open camera.")
 
@@ -16,6 +20,20 @@ def main() -> None:
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
+
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        face = face_classifier.detectMultiScale(
+            gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+        )
+
+        for (x, y, w, h) in face:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+
 
         cv2.imshow('Live Camera', frame)
 
